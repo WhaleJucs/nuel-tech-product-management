1685604428ce6d1c19865f9da2cf3e7d106edb13
Joao <57973684+WhaleJucs@users.noreply.github.com>
2026-02-06 21:43:32 -0300

    Ajuste nas rotas

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use('/products', productsRouter)
 
 // Iniciar o servidor
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
